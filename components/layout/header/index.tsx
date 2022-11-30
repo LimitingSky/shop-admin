f0835c8ec54d6632320259ebdb7e0b978f18daf5
Luis Garcia <un.garcia9719@gmail.com>
2022-11-29 22:54:29 -0600

    WIP: search bar

--- components/layout/header/index.tsx
+++ components/layout/header/index.tsx
@@ -1,6 +1,6 @@
 export const Nav = () => {
   return (
-    <div className="flex max-h-14 w-full flex-row items-center justify-between bg-gray-900 px-0 py-0">
+    <div className="flex max-h-14 w-full flex-row items-center justify-between bg-black px-0 py-0">
       <div className="flex flex-1 flex-row items-center">
         <div className="mx-20">logo</div>
         <div className="flex flex-1 flex-row text-white">
@@ -19,7 +19,7 @@ export const Nav = () => {
         </div>
       </div>
       <div className="flex flex-1 flex-row items-center justify-end">
-        <div className="flex flex-1 flex-row items-center justify-end mr-8">
+        <div className="flex flex-1 flex-row items-center justify-end mr-16">
           <a className="px-8" href="#">
           Client Name
         </a>
@@ -27,16 +27,16 @@ export const Nav = () => {
           Cart
         </a>
         </div>
-        <div className="bg-blue-700 py-4 px-10">
+        <div className="bg-purple-700 py-4 px-10 text-white pr-36">
           <a
             href="#"
-            className="px-4 py-5 text-gray-700 hover:bg-blue-900 hover:text-white"
+            className="px-4 py-5 hover:bg-blue-900 hover:text-white"
           >
             Man
           </a>
           <a
             href="#"
-            className="px-4 py-5 text-gray-700 hover:bg-blue-900 hover:text-white"
+            className="px-4 py-5 hover:bg-blue-900 hover:text-white"
           >
             Woman
           </a>
--- components/layout/header/search.tsx
+++ components/layout/header/search.tsx
@@ -2,7 +2,7 @@ import { AiOutlineSearch } from 'react-icons/ai'
 
 export const Search = () => {
     return(
-        <div className="flex bg-gray-50 justify-center items-center px-8 h-14 w-screen relative">
+        <div className="flex bg-white justify-center items-center px-8 h-14 w-screen relative shadow-lg shadow-slate-200">
             <div>
                 <a href="#" className="text-gray-700 uppercase px-4 py-4 hover:text-gray-900">Popular</a>
                 <a href="#" className="text-gray-700 uppercase px-4 py-4 hover:text-gray-900">Most Viewed</a>
@@ -10,7 +10,7 @@ export const Search = () => {
                 <a href="#" className="text-gray-700 uppercase px-4 py-4 hover:text-gray-900">Random</a>
             </div>
             <button className="absolute right-0 top-0 h-14 lg:mr-32 w-14 text-gray-700 hover:text-white hover:bg-black flex items-center justify-center">
-                <AiOutlineSearch className='text-2xl font-bold' />
+                <AiOutlineSearch className='text-3xl font-bold text-black' />
             </button>
         </div>
     )
